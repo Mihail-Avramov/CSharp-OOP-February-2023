8c570244d7846efd3ee1edeee19cd2641fbedb0d
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-02-24 16:07:49 +0200

    6

---
+++ 01_Inheritance_Exercise/Animals/Animal.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+		private string name;
+		private int age;
+		private string gender;
+
+        public Animal(string name, int age, string gender)
+        {
+            Name = name;
+			Age = age;
+			Gender = gender;
+        }
+
+		public string Gender
+		{
+            get => gender;
+            private init
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+                
+                gender = value;
+            }
+		}	
+
+
+		public int Age
+		{
+            get => age;
+            private init
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                age = value;
+            }
+		}
+
+
+		public string Name
+		{
+            get => name;
+            private init
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+					throw new ArgumentException("Invalid input!");
+                }
+
+                name = value;
+            }
+		}
+
+        public abstract string ProduceSound();
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(this.GetType().Name);
+            sb.AppendLine($"{Name} {Age} {Gender}");
+            sb.AppendLine(ProduceSound());
+
+            return sb.ToString().Trim();
+        }
+    }
+}
--- 01_Inheritance_Exercise/Animals/StartUp.cs
+++ 01_Inheritance_Exercise/Animals/StartUp.cs
@@ -1,10 +1,52 @@
-﻿namespace Animals
+﻿using System;
+
+namespace Animals
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "Beast!")
+            {
+                string[] animalArguments = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = animalArguments[0];
+                int age = int.Parse(animalArguments[1]);
+                string gender = animalArguments[2];
+
+                try
+                {
+                    switch (input)
+                    {
+                        case "Dog":
+                            Dog newDog = new Dog(name, age, gender);
+                            Console.WriteLine(newDog);
+                            break;
+                        case "Cat":
+                            Cat newCat = new Cat(name, age, gender);
+                            Console.WriteLine(newCat);
+                            break;
+                        case "Frog":
+                            Frog newFrog = new Frog(name, age, gender);
+                            Console.WriteLine(newFrog);
+                            break;
+                        case "Kitten":
+                            Kitten newKitten = new Kitten(name, age);
+                            Console.WriteLine(newKitten);
+                            break;
+                        case "Tomcat":
+                            Tomcat newTomcat = new Tomcat(name, age);
+                            Console.WriteLine(newTomcat);
+                            break;
+                    }
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+            }
         }
     }
 }
