dd9a44335aa7bf4f983b020cb413d96e78cc4832
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-02-24 15:19:28 +0200

    5

---
+++ 01_Inheritance_Exercise/Restaurant/Cake.cs
@@ -0,0 +1,13 @@
+﻿namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const double DefaultCakeGrams = 250;
+        private const double DefaultCakeCalories = 1000;
+        private const decimal DefaultCakePrice = 5M;
+
+        public Cake(string name) : base(name, DefaultCakePrice, DefaultCakeGrams, DefaultCakeCalories)
+        {
+        }
+    }
+}
---
+++ 01_Inheritance_Exercise/Restaurant/Coffee.cs
@@ -0,0 +1,15 @@
+﻿namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const double DefaultCoffeeMilliliters = 50;
+        private const decimal DefaultCoffeePrice = 3.5M;
+
+        public Coffee(string name, double caffeine) : base(name, DefaultCoffeePrice, DefaultCoffeeMilliliters)
+        {
+            Caffeine = caffeine;
+        }
+
+        public double Caffeine { get; private set; }
+    }
+}
