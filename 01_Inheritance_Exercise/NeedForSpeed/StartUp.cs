57f62ff7563591359ac4b20f3da0cb18e398e6e8
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-02-24 14:15:50 +0200

    4

--- 01_Inheritance_Exercise/NeedForSpeed/StartUp.cs
+++ 01_Inheritance_Exercise/NeedForSpeed/StartUp.cs
@@ -1,10 +1,19 @@
-﻿namespace NeedForSpeed
+﻿using System;
+
+namespace NeedForSpeed
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            
+            SportCar sportCar = new(111, 20);
+
+            Console.WriteLine($"Fuel before drive: {sportCar.Fuel} liters");
+            Console.WriteLine($"Fuel Consumption: {sportCar.FuelConsumption} liters per km");
+            Console.Write($"Enter the distance you want to drive in km: ");
+            int distance = int.Parse(Console.ReadLine());
+            sportCar.Drive(distance);
+            Console.WriteLine($"Fuel left: {sportCar.Fuel} liters");
         }
     }
 }
