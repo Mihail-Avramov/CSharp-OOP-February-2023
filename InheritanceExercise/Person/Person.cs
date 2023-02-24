e9f15cc37b8f4a9dd9d30c8369c57827ca32b7f9
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-02-24 09:50:12 +0200

    1
    1

---
+++ InheritanceExercise/Person/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Person
+{
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString() => $"Name: {Name}, Age: {Age}";
+    }
+}
