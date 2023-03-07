6b23582d4bd9dcc62245953bc4bd6f04156ce8fa
Mihail Avramov <115155730+Mihail-Avramov@users.noreply.github.com>
2023-03-07 15:29:38 +0200

    2

---
+++ 03_Interfaces_and_Abstraction_Exercise/T03.Telephony/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace T03.Telephony
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
